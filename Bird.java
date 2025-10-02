65275e9302fe4eda690c9c2f5e19265db74511ab
MuhammadQureshi724 <Mqureshi2@ggc.edu>
2025-10-02 01:45:06 -0400

    Update Bird.java

--- Bird.java
+++ Bird.java
@@ -2,9 +2,8 @@ package Flight;
 
 /**
  * Class: Bird
- * @author Muhammad Qureshi
- * @version 1.0
- * Course: ITEC XXXX Fall 2025
+ * Name: Muhammad Qureshi
+ * Course: ITEC section 5 Fall 2025
  * Written: October 1, 2025
  *
  * Represents a Bird that moves by flapping wings, walking, and jumping.
